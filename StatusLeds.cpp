fef202ba3e6db287bdcf140cb57cd7888b7e5bfc
preeve9534 <preeve@pdjr.eu>
2023-01-07 12:39:18 +0000

    Tweaks

--- StatusLeds.cpp
+++ StatusLeds.cpp
@@ -1,9 +1,9 @@
 #include "StatusLeds.h"
 
-StatusLeds::StatusLeds(unsigned int nleds, void (*callback)(unsigned char), unsigned long processInterval) {
+StatusLeds::StatusLeds(unsigned int nleds, unsigned long updateInterval, void (*callback)(unsigned char)) {
   this->nleds = nleds;
   this->callback = callback;
-  this->processInterval = processInterval;
+  this->updateInterval = updateInterval;
 
   this->leds = new LedState [this->nleds];
   for (unsigned int i = 0; i < this->nleds; i++) this->leds[i] = off;
@@ -23,7 +23,7 @@ unsigned char getStatus(bool performUpdate) {
       case off: case flashoff:
         status &= ~(0x01 << led);
         break;
-      case on: case once: case flashon:
+      default:
         status |= (0x01 << led);
         break;
     }
@@ -41,32 +41,35 @@ StatusLeds::LedState StatusLeds::getLedState(unsigned int led) {
     return(this->leds[led]);
 }
 
-void StatusLeds::update() {
-  for (unsigned int led = 0; led < this->nleds; led++) {
-    switch (this->leds[led]) {
-      case on: case off:
-        break;
-      case once:
-        this->leds[led] = off;
-        break;
-      case flash:
-        this->leds[led] = flashOn;
-        break;
-      case flashOn:
-        this->leds[led] = flashOff;
-      case flashOff:
-        this->leds[led] = flashOn;
-    }
-  }
-}
-
-void StatusLeds::process(bool performUpdate, bool force) {
+void StatusLeds::update(bool force, bool performCallback) {
   static unsigned long deadline = 0UL;
   unsigned long now = millis();
+  unsigned char status = this->getStatus();
 
   if ((now > deadline) || (force)) {
-      if (this->callback) this->callback(this->getStatus(performUpdate));
-      if (this->processInterval) deadline = (now + this->processInterval);
+    for (unsigned int led = 0; led < this->nleds; led++) {
+      switch (this->leds[led]) {
+        case on: case off:
+          break;
+        case once:
+          this->leds[led] = off;
+          break;
+        case twice:
+          this->leds[led] = once;
+          break;
+        case thrice:
+          this->leds[led] = twice;
+          break;
+        case flash:
+          this->leds[led] = flashOn;
+          break;
+        case flashOn:
+          this->leds[led] = flashOff;
+        case flashOff:
+          this->leds[led] = flashOn;
+      }
     }
+    if (this->updateInterval) deadline = (now + this->updateInterval);
   }
+  if ((performCallback) && (this->callback)) this->callback(status);
 }
--- StatusLeds.h
+++ StatusLeds.h
@@ -7,20 +7,19 @@
 class StatusLeds {
 
   public:
-    typedef enum eLedState { on, off, flash, once, flashOn, flashOff } LedState;
+    typedef enum eLedState { on, off, flash, once, twice, thrice, flashOn, flashOff } LedState;
 
-    StatusLeds(unsigned int nleds, void (*callback)(unsigned char) = 0, unsigned long processInterval = 200UL);
+    StatusLeds(unsigned int nleds = 8, unsigned long updateInterval = 200UL, void (*callback)(unsigned char) = 0);
     void setStatus(unsigned char status);
     unsigned char getStatus(bool performUpdate = false);
     void setLedState(unsigned int led, LedState state);
     StatusLeds::LedState getLedState(unsigned int led);
-    void update();
-    void process(bool performUpdate = true, bool force = false);
+    void update(bool force = false, performCallback = false);
 
   private:
     unsigned int nleds;
     void (*callback)(unsigned char);    
-    unsigned long processInterval;
+    unsigned long updateInterval;
     LedState *leds;
 
 };
